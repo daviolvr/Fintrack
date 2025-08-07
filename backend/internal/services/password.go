1cfaf0a634687452f18807bbfa2afc8bf9870cfd
Davi de Oliveira <olidavi031130@gmail.com>
2025-08-07 18:16:13 -0300

    feat: add hash for passwords and password check

---
+++ backend/internal/services/password.go
@@ -0,0 +1,17 @@
+package services
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Recebe uma senha em texto puro e retorna o hash gerado ou erro
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// Compara a senha em texto puro com o hash armazenado
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
